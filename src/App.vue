5f343d9c9fcc9297196c0a8a255347bdadc92b6e
austinlkillian <austin.l.killian@gmail.com>
2018-11-05 11:20:03 -0800

    git basic layout figured out

--- src/App.vue
+++ src/App.vue
@@ -30,8 +30,8 @@
             </ul>
           </div>
           <div id="icons">
-            <img />
-            <img />
+            <img src='./assets/searchOrange.png'/>
+            <img src='./assets/emailOrange.png'/>
           </div>
         </div>
       </header>
@@ -50,7 +50,21 @@
           <li>WEB DEV</li>
         </ul>
       </div>
+
+    <div id="contact">
+      <h1>FLUID</h1>
+      <h3>1065 South 500 West Bountiful, UT 84010 | (801) 295-9820 | Fluid Advertising</h3>
+      <div id="social">
+        <img src='./assets/facebookOrange.png'/>
+        <img src='./assets/twitterOrange.png'/>
+        <img src='./assets/emailOrange.png'/>
+        <img src='./assets/linkedInOrange.png'/>
+        <img src='./assets/blogOrange.png'/>
+      </div>
+      <p>Privacy Policy</p>
     </div>
+    </div>
+
   </div>
 </template>
 
@@ -62,38 +76,29 @@ export default {
   name: "app",
   data() {
     return {
-      navShow: false,
+      navShow: true,
       portList: [
         {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'https://www.gmc.com/content/dam/gmc/na/us/english/index/shared-assets/jellybeans/2018/sierra-ld/sierra-denali/2018-sierra-1500-denali-onyx-black.jpg?imwidth=162', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
-        {img:'http://getfluid.com/site/uploads/2016/01/fluid_sign_662x492_acf_cropped.jpg', co: 'FLUID', how: 'DESIGN | WEB DEV'},
+        {img:'https://www.gmc.com/content/dam/gmc/na/us/english/index/shared-assets/jellybeans/2018/sierra-ld/sierra-denali/2018-sierra-1500-denali-onyx-black.jpg?imwidth=162', co: 'GMC', how: 'DESIGN | WEB DEV'},
+        {img:'https://cdn.nextgov.com/media/img/upload/2018/05/16/051618microsoftNG/860x394.jpg', co: 'MICROSOFT', how: 'DESIGN | WEB DEV'},
+        {img:'https://fortunedotcom.files.wordpress.com/2015/08/gettyimages-140706886-1.jpg', co: 'IBM', how: 'DESIGN | WEB DEV'},
+        {img:'https://media.wired.com/photos/59c166e1fdf0d5611a915af7/2:1/w_2500,c_limit/AppleProductLaunch-HP-GettyImages-846153794.jpg', co: 'APPLE', how: 'DESIGN | WEB DEV'},
+        {img:'https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2015/05/mcdonalds-e1430922835284-1200x639.jpg', co: 'MCDONALDS', how: 'DESIGN | WEB DEV'},
+        {img:'https://brain-images-ssl.cdn.dixons.com/7/3/10170337/l_10170337_001.jpg', co: 'BEATS', how: 'DESIGN | WEB DEV'},
+        {img:'https://static.makeuseof.com/wp-content/uploads/2018/08/most-watched-youtube-670x335.jpg', co: 'YOUTUBE', how: 'DESIGN | WEB DEV'},
+        {img:'https://timedotcom.files.wordpress.com/2015/06/facebook.jpg', co: 'FACEBOOK', how: 'DESIGN | WEB DEV'},
+        {img:'https://cdn-images-1.medium.com/max/1600/1*yZ1LPIcXnnW6Ubmp2M-0rQ.png', co: 'AIRBNB', how: 'DESIGN | WEB DEV'},
+        {img:'https://www.geek.com/wp-content/uploads/2018/07/amazon-625x352.jpg', co: 'AMAZON', how: 'DESIGN | WEB DEV'},
+        {img:'https://amp.businessinsider.com/images/56b9fd532e526575008b5efb-750-562.jpg', co: 'WENDYS', how: 'DESIGN | WEB DEV'},
+        {img:'https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2016/12/twitch-e1491600114230-796x398.jpg', co: 'TWITCH', how: 'DESIGN | WEB DEV'},
+        {img:'https://brand.campaign.adidas.com/Images/adidas-skateboarding_SS18-CityCup-FranchiseTeaser-CLO_640x640_tcm66-268245.jpg?locale=en_US&device=desktop&version=2', co: 'ADIDAS', how: 'DESIGN | WEB DEV'},
+        {img:'http://www.sobhacitymall.com/wp-content/uploads/2017/10/Levis-1.png', co: 'LEVIS', how: 'DESIGN | WEB DEV'},
+        {img:'https://cdn.vox-cdn.com/thumbor/f3vZQsUgEUzG8J_SrwucezgvQQo=/0x0:1000x800/1200x800/filters:focal(420x320:580x480)/cdn.vox-cdn.com/uploads/chorus_image/image/58194493/IGN_logo_2.0.jpg', co: 'IGN', how: 'DESIGN | WEB DEV'},
+        {img:'https://slickdeals.net/blog/wp-content/uploads/2018/08/best_buy_anniversary_sale_52_hero_.jpg', co: 'BEST BUY', how: 'DESIGN | WEB DEV'},
+        {img:'https://assets.entrepreneur.com/content/3x2/2000/20171228231121-ent18-janfeb-greatclips.jpeg?width=700&crop=2:1', co: 'GREAT CLIPS', how: 'DESIGN | WEB DEV'},
+        {img:'https://cdn.gobankingrates.com/wp-content/uploads/2015/11/Home_depot_deals_best-848x477.jpg', co: 'HOME DEPOT', how: 'DESIGN | WEB DEV'},
+        {img:'https://amp.businessinsider.com/images/55a95c612acae7a6098b7998-750-563.jpg', co: 'BANK OF AMERICA', how: 'DESIGN | WEB DEV'},
+        {img:'https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/5b2a4a95562fa7f5948b8c5a/1529498266965/amc-now-offering-its-own-subscription-service-social.jpg?format=2500w', co: 'AMC', how: 'DESIGN | WEB DEV'}
       ]
     }
   },
@@ -139,6 +144,10 @@ $colors: (
     justify-content: center;
     align-items: center;
     z-index: 1;
+
+    #top:hover + header {
+      visibility: visible;
+  }
 
     #top-top {
     width: 100%;
@@ -213,7 +222,16 @@ $colors: (
     justify-content: space-evenly;
     flex-wrap: wrap;
 
+    header:hover {
+      visibility: visible;
+    }
+
+    header:hover {
+      visibility: visible;
+    }
+
     header {
+      // visibility: hidden;
       width: 100%;
       height: 150px;
       background: rgba(37, 37, 37, 0.776);
@@ -260,8 +278,6 @@ $colors: (
         img {
           width: 30px;
           height: 30px;
-          border-radius: 50%;
-          background: blue;
           margin: 15px;
         }
       }
@@ -293,6 +309,46 @@ $colors: (
           margin: 10px;
         }
       }
+    }
+  }
+
+  #contact {
+    width: 80%;
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-bottom: 100px;
+
+    h1 {
+      font-family: Arial;
+      font-weight: bold;
+      font-size: 35px;
+      color: rgb(59, 59, 59);
+    }
+
+    h3 {
+      font-family: arial;
+      font-weight: bold;
+      font-size: 25px;
+      color: grey;
+    }
+
+    #social {
+      width: 300px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      img {
+        width: 30px;
+      }
+    }
+
+    p {
+      font-weight: bold;
+      color: grey;
     }
   }
 
